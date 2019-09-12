820b934f9d7f6adee471dcd124dc83c83e0ced1f
Lucy Gao <real.lucy.gao@gmail.com>
2019-09-12 12:26:47 -0700

    started mathfun lab 3.1

---
+++ lab3.1mathfun/maths.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class maths here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class maths
+{
+    public static void main()
+    {
+        // solve the math problems
+        System.out.println("\u000C\nMATH PROBLEMS:");
+        int ans1 = 4 + 9;
+        System.out.println("4 + 9 = " + ans1);
+        
+        int ans2 = 46 / 7;
+        System.out.println("46 / 7 = " + ans2);
+        
+        int ans3 = 46 % 7;
+        System.out.println("46 % 7 = " + ans3);
+        
+        double ans4 = 2 * 3.0;
+        System.out.println("2 * 3.0 = " + ans4);
+        
+        double ans5 = (double)25 / 4;
+        System.out.println("(double)25 / 4 = " + ans5);
+        
+        int ans6 = (int)7.75 + 2;
+        System.out.println("(int)7.75 + 2 = " + ans6);
+        
+        int ans7 = (int)'P';
+        System.out.println("(int)'P' = " + ans7);
+        
+        char ans8 = (char)105;
+        System.out.println("(char)105 = " + ans8);
+    }
+}
